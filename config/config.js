fd67271ef92538c7bed16e28d5769f89bf265a8c
e <13544102930@163.com>
2019-12-29 15:09:02 +0800

    update

--- config/config.js
+++ config/config.js
@@ -132,7 +132,7 @@ export default {
             {
               path: '/comment',
               name: 'Comment',
-              icon: 'read',
+              icon: 'message',
               component: './comment',
             },
              {
--- config/defaultSettings.js
+++ config/defaultSettings.js
@@ -10,7 +10,7 @@ export default {
   menu: {
     locale: true,
   },
-  title: 'Ant Design Pro',
+  title: 'Fyodor Home',
   pwa: false,
   iconfontUrl: '',
 };
